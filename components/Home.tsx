8461a5cc619109af522769dc19d6a70f0a560e9a
devin mitchell <devin.a.mitchell@gmail.com>
2021-09-11 17:11:37 -0600

    navigation set up

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,19 +1,58 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function Home() {
+export default function Home({ navigation }) {
+
     return (
-        <View style={styles.container}>
-            <Text>Howdy</Text>
-        </View>
+        <ImageBackground source={require('../images/umbreon.jpg')} resizeMode='center' style={styles.bgImg}>
+            <View style={styles.titleWrap}>
+                <Text style={styles.title}>Pokemans</Text>
+            </View>
+            <View style={styles.wrapper}>
+                <TouchableOpacity onPress={() => navigation.push("Pokemon")}>
+                    <Text style={styles.buttonText}>Enter</Text>
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    bgImg: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+        alignItems: 'center'
+    },
+    titleWrap: {
+        position: 'absolute',
+        top:'10%',
+        width: '80%',
+        shadowColor: '#a88220', 
+        borderColor: '#a88220',
+        borderWidth: 3,
+        shadowOffset: { width: 0, height: 5},
+        shadowOpacity: .8,
+        shadowRadius: 30
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#a88220',
+        backgroundColor: 'rgba(0, 0, 0, 1)',
+        padding: 5,
+    },
+    wrapper: {
+        width: '80%', 
+        position: "absolute",
+        bottom: '10%',
+        paddingVertical: 10,
+        backgroundColor: '#a88220',
+        color: '#FFF',
+        borderRadius: 50,
+    },
+    buttonText: {
+        fontSize: 20,
+        textAlign: 'center',
+    },
+  });
---
+++ components/Pokemon.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function Pokemon() {
+    return (
+       <View>
+           <Text>Howdy Pokemaaaaaaan</Text>
+       </View>
+    )
+}
